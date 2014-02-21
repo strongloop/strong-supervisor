2f55d36a69606bd7b07fa9d3073fe1655ca9da19
Ryan Graham <ryan@strongloop.com>
2014-02-20 17:56:32 -0800

    Restore support for PORT and --port in example apps

--- test/module-app/index.js
+++ test/module-app/index.js
@@ -2,6 +2,10 @@ var options = require('optimist').argv;
 var express = require('express');
 var app = express();
 
+app.configure(function(){
+  app.set('port', options.port || process.env.PORT || 3000);
+});
+
 app.get('/', (function(){
   var started = new Date();
   return function(req,res) {
@@ -12,7 +16,7 @@ app.get('/', (function(){
   }
 })());
 
-var server = app.listen(0);
+var server = app.listen(app.get('port'));
 console.log('module-app listening on http://localhost:%d',
             server.address().port);
 console.log('argv: ', process.argv);
