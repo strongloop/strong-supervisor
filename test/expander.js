868ba2ce8cdd2d407c8244a4d2455206953935da
Ryan Graham <ryan@strongloop.com>
2014-08-01 12:38:07 -0700

    Add %a and %h to core string expander
    %a is expanded to appName and %h to hostname, assuming those keys exist
    on the worker object given to the expander.
    
    Parameters that are not provided should be ignored and not substituted.

--- test/expander.js
+++ test/expander.js
@@ -5,14 +5,29 @@ var assert = require('assert');
 var expander = require('../lib/expander');
 
 describe('expander.expand', function() {
-  var example_worker = { id: 1, pid: 1234 };
+  var example_worker = {
+    id: 1,
+    pid: 1234,
+    hostname: 'always-up',
+    appName: 'totally-awesome',
+  };
+  var falsey_worker = { id: 0, pid: null, hostname: undefined, appName: false };
   var examples = [
-    { given: 'supervisor.log', expect: 'supervisor.log' },
-    { given: 'myApp-%w-%p.log', expect: 'myApp-1-1234.log' },
+    { given: ['supervisor.log', example_worker],
+      expect: 'supervisor.log' },
+    { given: ['myApp-%w-%p.log', example_worker],
+      expect: 'myApp-1-1234.log' },
+    { given: ['%h.%a.%w[%p]', example_worker],
+      expect: 'always-up.totally-awesome.1[1234]' },
+    { given: ['%h.%a.%w[%p]', {}],
+      expect: '%h.%a.%w[%p]' },
+    { given: ['%h.%a.%w[%p]', falsey_worker],
+      expect: 'undefined.false.0[null]' },
   ];
+
   examples.forEach(function (e) {
     it('generates "' + e.expect + '" from "' + e.given + '"', function() {
-      var result = expander.expand(e.given, example_worker);
+      var result = expander.expand.apply(null, e.given);
       assert.equal(result, e.expect);
     });
   });
