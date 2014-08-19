633ef38fd96cae39ad249f23cd1142730c2513d1
Sam Roberts <sam@strongloop.com>
2014-08-19 14:42:20 -0700

    run: support control and exit via node ipc
    All commands available to `slc runctl` over local domain sockets are
    made available to a parent node process, if it establishes an ipc
    connection to the supervisor (see `child_process.fork()`).
    
    Also, when the ipc connection is disconnected (either explicitly, or
    because the parent process exited, the child can't tell the difference),
    the supervisor will exit to prevent process leaks.

--- bin/sl-run.js
+++ bin/sl-run.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
+// Exit on loss of parent process, if it had established an ipc control channel.
+// We do this ASAP because we don't want child processes to leak, outliving
+// their parent. If the parent has not established an 'ipc' channel to us, this
+// will be a no-op, the disconnect event will never occur.
+process.on('disconnect', function() {
+  process.exit(2);
+});
+
 var config = require('../lib/config'); // May exit, depending on argv
 var log = config.logger;
 
