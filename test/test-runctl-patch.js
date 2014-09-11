32e2a5b1014d79b3f775c008e3208150df082527
Sam Roberts <sam@strongloop.com>
2014-09-11 14:03:05 -0700

    runctl: support patching metrics in dynamically
    strong-agent-statsd > 1.0.0 is needed to support reporting stats ending
    in .timer as millisecond timing values.

---
+++ test/test-runctl-patch.js
@@ -0,0 +1,32 @@
+var helper = require('./helper');
+
+if (helper.skip()) return;
+
+var rc = helper.runCtl;
+var expect = rc.expect;
+var failon = rc.failon;
+var supervise = rc.supervise;
+var waiton = rc.waiton;
+
+var app = require.resolve('./module-app');
+
+var run = supervise(app);
+
+// supervisor should exit with 0 after we stop it
+run.on('exit', function(code, signal) {
+  assert.equal(code, 0);
+});
+
+cd(path.dirname(app));
+
+waiton('', /worker count: 0/);
+
+// Just test the patch command communication, particularly what happens when
+// `--metrics` was not provided as an option to `slc run` (the actual behaviour
+// of applied patches is tested in strong-agent).
+fs.writeFileSync('_patch.json', '{"no-such-file-anywhere-i-hope":[]}');
+failon('patch 0 _patch.json', /not configured to report metrics/);
+
+expect('stop');
+
+helper.pass = true;
