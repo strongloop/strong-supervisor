a1f3fc7e8b1c20d77a8a13af34e0c01f78a72851
Krishna Raman <kraman@gmail.com>
2014-11-08 10:08:34 -0800

    Add IPC notifications for tracking and profiling

---
+++ test/test-ipcctl-notifications.js
@@ -0,0 +1,155 @@
+var helper = require('./helper');
+
+if (helper.skip()) return;
+
+var assert = require('assert');
+var async = require('async');
+var control = require('strong-control-channel/process');
+var cp = require('child_process');
+var debug = require('./debug');
+var ee = new (require('events').EventEmitter)();
+
+var options = {stdio: [0, 1, 2, 'ipc']};
+var run = require.resolve('../bin/sl-run');
+var yes = require.resolve('./v1-app');
+var args = [
+  run,
+  '--cluster=0',
+  '--no-profile',
+  yes
+];
+var run = cp.spawn(process.execPath, args, options);
+
+function onRequest(req, cb) {
+  debug(req);
+  ee.emit(req.cmd, req);
+  cb();
+}
+
+var ctl = control.attach(onRequest, run);
+
+async.series([
+  start,
+  scaleUp,
+  scaleDown,
+  startCpuProfiling,
+  stopCpuProfiling,
+  startObjTracking,
+  stopObjTracking,
+  heapDump,
+  disconnect,
+], function(er) {
+  assert.ifError(er);
+  helper.pass = true;
+  process.exit(0);
+});
+
+function start(cb) {
+  ee.once('started', function(n) {
+    assert(typeof n.agentVersion === 'string', 'Agent version should be present');
+    assert(n.pid > 0, 'Master pid should be present');
+    cb();
+  });
+}
+
+function scaleUp(cb) {
+  ctl.request({cmd: 'set-size', size: 2}, function(rsp) {
+    var forked = false;
+    var listening = false;
+
+    ee.once('fork', function(n) {
+      forked = true;
+      assert(n.id > 0, 'Worker ID should be present');
+      assert(n.pid > 0, 'Worker pid should be present');
+      if (forked && listening) cb();
+    });
+
+    ee.once('listening', function(n) {
+      assert(n.id > 0, 'Worker ID should be present');
+      assert(n.address !== undefined, 'Worker endpoint should be present');
+      listening = true;
+      if (forked && listening) cb();
+    });
+  });
+}
+
+function scaleDown(cb) {
+  ctl.request({cmd: 'set-size', size: 1}, function(rsp) {
+    ee.once('exit', function(n) {
+      assert(n.id > 0, 'Worker ID should be present');
+      assert(n.pid > 0, 'Worker pid should be present');
+      assert(n.reason !== undefined, 'Worker exit reason should be present');
+      cb();
+    });
+  });
+}
+
+function startCpuProfiling(cb) {
+  ee.once('cpu-profiling', function(n) {
+    assert(n.id > 0, 'Worker ID should be present');
+    assert(n.isRunning === true, 'Profiling should be running');
+    cb();
+  });
+
+  ctl.request({cmd: 'start-cpu-profiling', target: 1}, function(rsp) {
+    assert(!rsp.error);
+  });
+}
+
+function stopCpuProfiling(cb) {
+  ee.once('cpu-profiling', function(n) {
+    assert(n.id > 0, 'Worker ID should be present');
+    assert(n.isRunning === false, 'Profiling should not be running');
+    cb();
+  });
+
+  var req = {cmd: 'stop-cpu-profiling', target: 1, filePath: 'test.cpuprofile'};
+  ctl.request(req, function(rsp) {
+    assert(!rsp.error);
+  });
+}
+
+function startObjTracking(cb) {
+  ee.once('object-tracking', function(n) {
+    assert(n.id > 0, 'Worker ID should be present');
+    assert(n.isRunning === true, 'Profiling should be running');
+    cb();
+  });
+
+  ctl.request({cmd: 'start-tracking-objects', target: 1}, function(rsp) {
+    assert(!rsp.error);
+  });
+}
+
+function stopObjTracking(cb) {
+  ee.once('object-tracking', function(n) {
+    assert(n.id > 0, 'Worker ID should be present');
+    assert(n.isRunning === false, 'Profiling should not be running');
+    cb();
+  });
+
+  ctl.request({cmd: 'stop-tracking-objects', target: 1}, function(rsp) {
+    assert(!rsp.error);
+  });
+}
+
+function heapDump(cb) {
+  ee.once('heap-snapshot', function(n) {
+    assert(n.id > 0, 'Worker ID should be present');
+    assert(n.isRunning === false, 'Dump should not be running');
+    cb();
+  });
+
+  var req = {cmd: 'heap-snapshot', target: 1, filePath: 'test.heapsnapshot'};
+  ctl.request(req, function(rsp) {
+    assert(!rsp.error);
+  });
+}
+
+function disconnect(done) {
+  run.disconnect();
+  run.on('exit', function(status) {
+    assert.equal(status, 2);
+    return done();
+  });
+}
