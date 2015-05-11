54a2c1caa7594b2fb7897527c5c48971b9d5c9c2
Ryan Graham <ryan@strongloop.com>
2015-05-11 12:06:25 -0700

    test: fix test-run-trace

--- test/test-run-trace.js
+++ test/test-run-trace.js
@@ -1,11 +1,8 @@
-var helper = require('./helper');
-if (helper.skip()) return;
-helper.pass = true; // Use tap, not this check.
-
-var http = require('http');
-var tap = require('tap');
 var debug = require('./debug');
+var helper = require('./helper');
+var http = require('http');
 var run = helper.runWithControlChannel;
+var tap = require('tap');
 
 tap.test('traces are forwarded via parentCtl', function(t) {
   t.plan(2);
@@ -23,6 +20,7 @@ tap.test('traces are forwarded via parentCtl', function(t) {
       }
     }
   );
+  app.ref();
   app.on('exit', function(code, signal) {
     debug('supervisor exit: %s', signal || code);
     t.end();
