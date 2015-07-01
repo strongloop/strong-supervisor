89d3b0df7577e5082a5e64442d38bb60a3f2a4e4
Setogit <setogit@gmail.com>
2015-07-01 12:28:34 -0700

    Implement Capabilities API
    watchdog/smart-profiling, metrics, cpuprofil, heapshanshot, patch, tracing
    add test-capabilities.js
    list API: return an empty array if no failures

---
+++ test/test-capablities.js
@@ -0,0 +1,27 @@
+var capabilities = require('../lib/capabilities');
+var tap = require('tap');
+
+tap.test('targetctl capabilities', function(t) {
+  var caps = ['watchdog', 'tracing', 'metrics',
+      'cpuprofile', 'heapsnapshot', 'patch'];
+  var list = capabilities.list();
+  t.match(list, caps, 'Capabilities are defined.');
+  list.forEach(function(f) {
+    t.test('Capabilities for ' + f, function(t) {
+      capabilities.query(f, function(result, reasons) {
+        t.comment('Feature %j: %j, %j', f, result, reasons);
+        t.ok(result || reasons.length, 'returned success or list of failures');
+        t.end();
+      });
+    });
+  });
+
+  // heapsnapshot should work everywhere these tests run
+  t.test('heapsnapshot', function(t) {
+    capabilities.query('heapsnapshot', function(result, reasons) {
+      t.ok(result, 'heapsnapshot should be available');
+      t.equal(reasons.length, 0, 'heapsnapshot should have no failure reasons');
+      t.end();
+    });
+  });
+});
