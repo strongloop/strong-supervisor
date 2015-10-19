ff9419060da520f45b10fe7c7413e5f3165492ea
Ryan Graham <ryan@strongloop.com>
2015-10-19 10:19:33 -0700

    lint: update to eslint@1 and strongloop rules

--- test/module-app/index.js
+++ test/module-app/index.js
@@ -4,17 +4,17 @@ var app = express();
 
 app.set('port', options.port || process.env.PORT || 0);
 
-app.get('/', (function(){
+app.get('/', (function() {
   var started = new Date();
-  return function(req,res) {
+  return function(req, res) {
     res.send({
       started: started,
       uptime: (Date.now() - Number(started)) / 1000
     });
-  }
+  };
 })());
 
-app.get('/delay', function(req, res){
+app.get('/delay', function(req, res) {
   setTimeout(function() { // line 18
     res.send('OK\n'); // line 19
   }, Math.random() * 100);
@@ -23,12 +23,12 @@ app.get('/delay', function(req, res){
 var server = app.listen(app.get('port'), function() {
   console.log('module-app listening on http://localhost:%d',
               server.address().port);
-  process.argv.forEach(function (v, i, a) {
+  process.argv.forEach(function(v, i, a) {
     console.log('argv %d: %s', i, v);
   });
   console.log('env VAR="%s"', process.env.VAR);
 
-  Object.keys(process.env).forEach(function (v, i, a) {
+  Object.keys(process.env).forEach(function(v, i, a) {
     if (/STRONGLOOP/.test(v) || /SL/.test(v))
       console.log('env %s="%s"', v, process.env[v]);
   });
