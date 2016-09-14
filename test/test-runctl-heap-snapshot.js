099a4aa4951c1763d22fadc3be0d94ccdf9e46ba
Sam Roberts <vieuxtech@gmail.com>
2016-09-14 12:50:44 -0700

    test: name heap snapshot tests

--- test/test-runctl-heap-snapshot.js
+++ test/test-runctl-heap-snapshot.js
@@ -26,37 +26,37 @@ tap.test('runctl heap snapshot', function(t) {
     t.end();
   });
 
-  t.doesNotThrow(function() {
+  t.doesNotThrow('cd', function() {
     cd(path.dirname(APP));
   });
 
-  t.doesNotThrow(function() {
+  t.doesNotThrow('no arg', function() {
     waiton('', /worker count: 0/);
   });
-  t.doesNotThrow(function() {
+  t.doesNotThrow('set-size', function() {
     expect('set-size 1');
   });
-  t.doesNotThrow(function() {
+  t.doesNotThrow('status worker count', function() {
     waiton('status', /worker count: 1/);
   });
-  t.doesNotThrow(function() {
+  t.doesNotThrow('status worker id', function() {
     expect('status', /worker id 1:/);
   });
 
-  t.doesNotThrow(function() {
+  t.doesNotThrow('heap snapshot 0', function() {
     expect('heap-snapshot 0', /node\.0.*\.heapsnapshot/);
   });
-  t.doesNotThrow(function() {
+  t.doesNotThrow('heap snapshot 1', function() {
     expect('heap-snapshot 1', /node\.1.*\.heapsnapshot/);
   });
 
-  t.doesNotThrow(function() {
+  t.doesNotThrow('heap snapshot 1 foo', function() {
     expect('heap-snapshot 1 ' + name, /foo.*\.heapsnapshot/);
   });
-  t.doesNotThrow(function() {
+  t.doesNotThrow('heap snapshot 1 does/not/exist', function() {
     failon('heap-snapshot 1 /does/not/exist', /ENOENT/);
   });
-  t.doesNotThrow(function() {
+  t.doesNotThrow('stop', function() {
     expect('stop');
   });
 });
