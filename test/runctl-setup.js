1736494a0ab3ad9b64b4d1f0bbacd84b9f5a1e85
Sam Roberts <vieuxtech@gmail.com>
2016-09-15 13:13:48 -0700

    test: factor runctl setup into a helper

---
+++ test/runctl-setup.js
@@ -0,0 +1,24 @@
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: strong-supervisor
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+
+var path = require('path');
+var supervise = require('./supervise');
+
+module.exports = setup;
+
+function setup(t, args) {
+  var app = require.resolve('./module-app');
+  var run = supervise(app, args);
+
+  // supervisor should exit with 0 after we stop it
+  run.on('exit', function(code, signal) {
+    t.equal(signal || code, 0);
+    t.end();
+  });
+
+  process.chdir(path.dirname(app));
+}
