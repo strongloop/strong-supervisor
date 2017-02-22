e10e22267063c6c904eb3bf6f737812ee92fece0
Sam Roberts <vieuxtech@gmail.com>
2017-02-22 12:58:15 -0800

    Update copyright years

--- bin/sl-runctl.js
+++ bin/sl-runctl.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// Copyright IBM Corp. 2014,2015. All Rights Reserved.
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
 // Node module: strong-supervisor
 // This file is licensed under the Artistic License 2.0.
 // License text available at https://opensource.org/licenses/Artistic-2.0
